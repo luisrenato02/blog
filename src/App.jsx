93dbfd0eea9c223a92a370e30849457d3b27badc
Luís Renato <89888509+luisrenato02@users.noreply.github.com>
2023-02-07 20:15:14 -0300

    iniciação

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { App } from "App.css";
+import { Outlet } from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <div className="container">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider, Router } from "react-router-dom";
+//pages
+import { Home } from "./routes/Home";
+import { NewPost } from "./routes/NewPost";
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/new",
+        element: <NewPost />
+      },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
